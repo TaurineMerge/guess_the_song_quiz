c25c1895a7ca48cefaf3ec05755f87b3bc7003ba
Vladislav <plotnikov12341@gmail.com>
2026-05-15 23:27:13 +0300

    chore: update users base dto

---
+++ server/src/common/dto/users-base.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+
+export class UsersBaseDto {
+  @IsString()
+  @MinLength(3)
+  username: string;
+
+  @IsEmail()
+  @IsOptional()
+  email?: string;
+
+  @MinLength(6)
+  @IsOptional()
+  password: string;
+}
