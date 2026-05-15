4e86be422e4d3e9a024baa67c19e337346af914b
Vladislav <plotnikov12341@gmail.com>
2026-05-15 19:40:54 +0300

    feat: add users database schema

---
+++ server/src/users/schema.ts
@@ -0,0 +1,8 @@
+import { pgTable, varchar, uuid } from 'drizzle-orm/pg-core';
+
+export const usersSchema = pgTable('users', {
+  userId: uuid('user_id').primaryKey().defaultRandom(),
+  username: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).unique(),
+  passwordHash: varchar('password_hash', { length: 255 }),
+});
