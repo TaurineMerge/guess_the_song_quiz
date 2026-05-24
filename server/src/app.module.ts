4448f27e86d93d018dff269433755044dfb1dd68
Vladislav <plotnikov12341@gmail.com>
2026-05-25 00:46:42 +0300

    chore: add jwt module import to app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './common/infrastructure/database/database.module';
+import { JwtModule } from '@nestjs/jwt';
+import jwtConfig from './common/infrastructure/config/jwt.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
     DatabaseModule,
   ],
   controllers: [],
