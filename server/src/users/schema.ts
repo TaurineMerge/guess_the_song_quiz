7f3b16aed881a7e876b25bbdfc8dacd4f2525d44
Vladislav <plotnikov12341@gmail.com>
2026-05-26 00:33:05 +0300

    chore: rename drizzle user schema

--- server/src/users/schema.ts
+++ server/src/users/schema.ts
@@ -1,6 +1,6 @@
 import { pgTable, varchar, uuid } from 'drizzle-orm/pg-core';
 
-export const usersSchema = pgTable('users', {
+export const users = pgTable('users', {
   userId: uuid('user_id').primaryKey().defaultRandom(),
   username: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).unique(),
