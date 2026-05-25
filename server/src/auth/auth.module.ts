ccad4baba53b1daf063bf201f0e20a02c26efdda
Vladislav <plotnikov12341@gmail.com>
2026-05-26 00:30:52 +0300

    chore: add database module import to auth module

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PasswordHasher } from './ports/password-hasher.port';
 import { BcryptPasswordHasher } from './infrastructure/crypto/password-hasher-bcrypt.adapter';
+import { DatabaseModule } from 'src/common/infrastructure/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [
     {
       provide: PasswordHasher,
