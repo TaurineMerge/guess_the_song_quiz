5be3579fd2603a88f381095b5502ff7d3b8e913f
Vladislav <plotnikov12341@gmail.com>
2026-05-26 15:35:05 +0300

    chore: add jwt module import and config module import

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -6,9 +6,17 @@ import { APP_GUARD } from '@nestjs/core';
 import { AuthenticationGuard } from './guards/authentication.guard';
 import { AccessTokenGuard } from './guards/access-token.guard';
 import { AuthService } from './domain/auth.service';
+import jwtConfig from './config/jwt.config';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './presenter/http/auth.controller';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    ConfigModule.forFeature(jwtConfig),
+  ],
   providers: [
     {
       provide: PasswordHasher,
@@ -21,5 +29,6 @@ import { AuthService } from './domain/auth.service';
     AccessTokenGuard,
     AuthService,
   ],
+  controllers: [AuthController],
 })
 export class AuthModule {}
