725d2c41b35558a9a56fc7180ef42f2c8dd39892
Vladislav <plotnikov12341@gmail.com>
2026-06-05 21:44:07 +0300

    feat: implement import playlist service method

--- server/src/playlists/domain/playlist.service.ts
+++ server/src/playlists/domain/playlist.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PlaylistSongsDto } from '../presenter/http/dto/playlist-songs-metadata.dto';
+import { YoutubeService } from 'src/youtube/domain/youtube.service';
 
 @Injectable()
 export class PlaylistService {
-  constructor() {}
+  constructor(private readonly youtubeService: YoutubeService) {}
 
   createPlaylist(userId: string, playlistSongs: PlaylistSongsDto) {}
   findPlaylist(userId: string, playlistId: string) {}
-  importPlaylist(userId: string, playlistId: string) {}
+  async importPlaylist(userId: string, playlistId: string) {
+    return await this.youtubeService.getPlaylist(userId, playlistId);
+  }
 }
