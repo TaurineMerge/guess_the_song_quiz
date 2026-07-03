6b1e1fc1b1b75955884912945947f65e1b8fb961
Vladislav <plotnikov12341@gmail.com>
2026-07-03 12:43:39 +0300

    chore: enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
