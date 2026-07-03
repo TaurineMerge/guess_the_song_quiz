1da07051c19d0f8cf6e6af33bae5295f7200a63a
Vladislav <plotnikov12341@gmail.com>
2026-07-03 14:03:35 +0300

    feat: add room lobby route

--- web/src/app/App.tsx
+++ web/src/app/App.tsx
@@ -1,10 +1,18 @@
 import { RoomLobby } from "./rooms/components/room-lobby/room-lobby";
 import styles from "./App.module.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/rooms/:roomId",
+    element: <RoomLobby />,
+  },
+]);
 
 export function App() {
   return (
     <div className={styles.page}>
-      <RoomLobby />
+      <RouterProvider router={router} />
     </div>
   );
 }
