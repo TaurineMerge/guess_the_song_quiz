45d29d26c91ebd83306f7e6afba513647aa6ca0e
Vladislav <plotnikov12341@gmail.com>
2026-07-04 22:24:37 +0300

    feat: add rooms websocket gateway

--- server/src/rooms/presenter/ws/rooms.gateway.ts
+++ server/src/rooms/presenter/ws/rooms.gateway.ts
@@ -1,17 +1,21 @@
-import { JwtService } from '@nestjs/jwt';
 import {
   ConnectedSocket,
   MessageBody,
-  OnGatewayConnection,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
-import { Socket } from 'node_modules/socket.io/dist/socket';
+import { Socket } from 'socket.io';
 import { roomClientEvents } from 'shared/rooms/events/room-client-events';
 import { roomServerEvents } from 'shared/rooms/events/room-server-events';
 import { AnswerResultPayload } from 'shared/rooms/types/room.types';
-import { JwtPayload } from 'src/auth/interfaces/jwt-payload.interface';
+import { UseGuards } from '@nestjs/common';
+import { RoomParticipantGuard } from 'src/rooms/guards/room-participant.guard';
+import { WsJwtGuard } from 'src/rooms/guards/ws-jwt.guard';
+import { RoomsService } from 'src/rooms/domain/rooms.service';
+import { ActiveUser } from 'src/rooms/decorators/active-user.decorator';
+import { RoomMapper } from 'src/rooms/domain/room-mapper';
 
+@UseGuards(WsJwtGuard)
 @WebSocketGateway({
   cors: {
     origin: process.env.CORS_ORIGIN,
@@ -19,55 +23,95 @@ import { JwtPayload } from 'src/auth/interfaces/jwt-payload.interface';
   },
   transports: ['polling', 'websocket'],
 })
-export class RoomsGateway implements OnGatewayConnection {
-  constructor(private jwtService: JwtService) {}
+export class RoomsGateway {
+  constructor(
+    private roomsService: RoomsService,
+    private roomMapper: RoomMapper,
+  ) {}
 
+  @UseGuards(RoomParticipantGuard)
   @SubscribeMessage(roomClientEvents.SUBMIT_ANSWER)
-  handleAnswer(
+  async handleAnswer(
+    @ActiveUser('sub') userId: string,
     @MessageBody() data: { roomId: string; answer: string },
     @ConnectedSocket() client: Socket,
   ) {
-    // TODO: answer validation
+    const { roomId, answer } = data;
+
+    const isRightAnswer = await this.roomsService.checkAnswer(
+      roomId,
+      userId,
+      answer,
+    );
+
     const result: AnswerResultPayload = {
-      playerId: client.data.user.sub,
-      roomId: data.roomId,
-      status: 'correct',
+      playerId: userId,
+      roomId,
+      status: isRightAnswer,
     };
 
     client.to(data.roomId).emit(roomServerEvents.ANSWER_RESULT, result);
   }
 
+  @UseGuards(RoomParticipantGuard)
   @SubscribeMessage(roomClientEvents.LEAVE_ROOM)
   async handleLeaving(
-    @MessageBody('roomId') roomId: string,
+    @ActiveUser('sub') userId: string,
+    @MessageBody() data: { roomId: string },
     @ConnectedSocket() client: Socket,
   ): Promise<void> {
+    const roomId = data.roomId;
     await client.leave(roomId);
+
+    const player = await this.roomsService.removePlayer(roomId, userId);
+
     client.to(roomId).emit(roomServerEvents.PLAYER_LEFT, {
-      id: client.data.user.sub,
-      // остальные поля игрока из БД
+      id: player.id,
+      name: player.name,
     });
   }
 
-  @SubscribeMessage(roomClientEvents.JOIN_ROOM)
-  async handleJoin(
-    @MessageBody('roomId') roomId: string,
+  @UseGuards(RoomParticipantGuard)
+  @SubscribeMessage(roomClientEvents.ROOM_STATE)
+  async handleRoomState(
+    @MessageBody() data: { roomId: string },
     @ConnectedSocket() client: Socket,
-  ): Promise<void> {
-    await client.join(roomId);
-    client.to(roomId).emit(roomServerEvents.PLAYER_JOINED, {
-      id: client.data.user.sub,
-      // остальные поля игрока из БД
+  ): Promise<void | null> {
+    const roomId = data.roomId;
+    const room = await this.roomsService.getRoomState(roomId);
+    const roomDto = this.roomMapper.toDto(room);
+    client.emit(roomServerEvents.ROOM_STATE, roomDto);
+  }
+
+  @UseGuards(RoomParticipantGuard)
+  @SubscribeMessage(roomClientEvents.ANSWER_LOCK)
+  async handleAnswerLock(
+    @ActiveUser('sub') userId: string,
+    @MessageBody() data: { roomId: string },
+    @ConnectedSocket() client: Socket,
+  ): Promise<void | null> {
+    const roomId = data.roomId;
+
+    const player = await this.roomsService.setLock(roomId, userId);
+
+    client.to(roomId).emit(roomServerEvents.ANSWER_LOCK, {
+      id: player.id,
+      name: player.name,
     });
   }
 
-  handleConnection(client: Socket): void {
-    try {
-      const token = client.handshake.auth.token as string;
-      const user = this.jwtService.verify<Pick<JwtPayload, 'sub'>>(token);
-      client.data.user = user;
-    } catch {
-      client.disconnect();
-    }
+  @UseGuards(RoomParticipantGuard)
+  @SubscribeMessage(roomClientEvents.JOIN_ROOM)
+  async handleRoomJoin(
+    @ActiveUser('sub') userId: string,
+    @MessageBody() data: { roomId: string },
+    @ConnectedSocket() client: Socket,
+  ): Promise<void | null> {
+    const roomId = data.roomId;
+    await client.join(roomId);
+
+    const player = await this.roomsService.addPlayer(roomId, userId);
+
+    client.to(roomId).emit(roomServerEvents.PLAYER_JOINED, player);
   }
 }
