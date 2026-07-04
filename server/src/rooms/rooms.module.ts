2c65a523ea3ae6de0733fb04f01aca520af3d37b
Vladislav <plotnikov12341@gmail.com>
2026-07-04 22:36:31 +0300

    chore: update rooms module dependencies

--- server/src/rooms/rooms.module.ts
+++ server/src/rooms/rooms.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { RoomsGateway } from './presenter/ws/rooms.gateway';
+import { JwtService } from '@nestjs/jwt';
+import { RoomStorage } from './domain/ports/room-storage.port';
+import { InMemoryStorage } from './infrastructure/storage/in-memory.storage';
+import { RoomsService } from './domain/rooms.service';
+import { RoomMapper } from './domain/room-mapper';
+import { DatabaseModule } from 'src/common/infrastructure/database/database.module';
 
 @Module({
-  providers: [RoomsGateway],
+  imports: [DatabaseModule],
+  providers: [
+    RoomsGateway,
+    JwtService,
+    {
+      provide: RoomStorage,
+      useClass: InMemoryStorage,
+    },
+    RoomsService,
+    RoomMapper,
+  ],
   exports: [RoomsGateway],
 })
 export class RoomsModule {}
