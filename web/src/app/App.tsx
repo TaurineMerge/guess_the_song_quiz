8f939e393c8ca571dc0c008f8a6ab2737a24a723
Vladislav <plotnikov12341@gmail.com>
2026-07-05 19:50:20 +0300

    feat: add login and signup routes, add session store initialization

--- web/src/app/App.tsx
+++ web/src/app/App.tsx
@@ -1,13 +1,29 @@
 import { RoomLobby } from "./rooms/components/room-lobby/room-lobby";
 import styles from "./App.module.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import { SignIn } from "./auth/components/sign_in/sign-in";
+import { SignUp } from "./auth/components/sign_up/sign-up";
+import { useEffect } from "react";
+import { useSessionStore } from "./auth/store/session-store";
 
 const router = createBrowserRouter([
   {
     path: "/rooms/:roomId",
     element: <RoomLobby />,
   },
+  {
+    path: "/login",
+    element: <SignIn />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
 ]);
+
+useEffect(() => {
+  useSessionStore.getState().initFromStorage();
+}, []);
 
 export function App() {
   return (
