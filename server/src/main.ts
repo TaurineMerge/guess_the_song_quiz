15fc524ec7105cfcba3cc1e69f0dfb51d8db6681
Vladislav <plotnikov12341@gmail.com>
2026-07-05 19:52:47 +0300

    chore: add cors configuration

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,7 +8,13 @@ async function bootstrap() {
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    credentials: true,
+    optionsSuccessStatus: 204,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
