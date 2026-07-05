0c4ea3531cc230b6355137ef381daa5cfef6688c
Vladislav <plotnikov12341@gmail.com>
2026-07-05 19:52:09 +0300

    chore: import jwt config

--- server/src/rooms/rooms.module.ts
+++ server/src/rooms/rooms.module.ts
@@ -1,17 +1,22 @@
 import { Module } from '@nestjs/common';
 import { RoomsGateway } from './presenter/ws/rooms.gateway';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { RoomStorage } from './domain/ports/room-storage.port';
 import { InMemoryStorage } from './infrastructure/storage/in-memory.storage';
 import { RoomsService } from './domain/rooms.service';
 import { RoomMapper } from './domain/room-mapper';
 import { DatabaseModule } from 'src/common/infrastructure/database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from 'src/auth/config/jwt.config';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    ConfigModule.forFeature(jwtConfig),
+  ],
   providers: [
     RoomsGateway,
-    JwtService,
     {
       provide: RoomStorage,
       useClass: InMemoryStorage,
